bcbc33b05a6383fd8ff23a8bf589e8b2c0f8a5dc
Jarett Sisk <jarett.sisk@gmail.com>
2025-07-07 15:37:12 -0500

    Added some jsDoc.

--- src/components/GridContainer/GridContainer.jsx
+++ src/components/GridContainer/GridContainer.jsx
@@ -1,5 +1,18 @@
 import "./GridContainer.css";
 
+/**
+ * A flexible grid container component that wraps children in a responsive flexbox layout.
+ * @param {Object} props - Component props
+ * @param {React.ReactNode} props.children - Child elements to render inside the container
+ * @param {string} [props.className=""] - Additional CSS classes to apply
+ * @param {string} [props.gap="8px"] - Gap between grid items (CSS gap value)
+ * @param {string} [props.justifyContent="center"] - Flex justify-content alignment
+ * @param {string} [props.alignItems="stretch"] - Flex align-items alignment
+ * @param {string} [props.flexDirection="row"] - Flex direction of the container
+ * @param {string} [props.padding="0"] - Padding inside the container
+ * @param {...Object} props.otherProps - Additional props passed to the container div
+ * @returns {JSX.Element} Rendered grid container
+ */
 export const GridContainer = (props) => {
     const {
         children,
@@ -11,7 +24,6 @@ export const GridContainer = (props) => {
         padding = "0",
         ...otherProps
     } = props;
-
 
     const containerStyle = {
         display: 'flex',
--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -1,5 +1,18 @@
 import "./GridItem.css";
 
+/**
+ * A responsive grid item component that applies size classes based on breakpoints.
+ * @param {Object} props - Component props
+ * @param {React.ReactNode} props.children - Child elements to render inside the grid item
+ * @param {number} [props.xs] - Grid size for extra small screens (applies xs-{value} class)
+ * @param {number} [props.sm] - Grid size for small screens (applies sm-{value} class)
+ * @param {number} [props.md] - Grid size for medium screens (applies md-{value} class)
+ * @param {number} [props.lg] - Grid size for large screens (applies lg-{value} class)
+ * @param {number} [props.xl] - Grid size for extra large screens (applies xl-{value} class)
+ * @param {string} [props.className=""] - Additional CSS classes to apply
+ * @param {...Object} props.props - Additional props passed to the grid item div
+ * @returns {JSX.Element} Rendered grid item with responsive classes
+ */
 export const GridItem = ({
                              children,
                              xs,
@@ -11,7 +24,7 @@ export const GridItem = ({
                              ...props
                          }) => {
     const classes = [
-        'grid-item',
+        'grid-item-flex',
         xs && `xs-${xs}`,
         sm && `sm-${sm}`,
         md && `md-${md}`,
